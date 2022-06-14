4d3d9c3b5b067e66731bcbb2dc8bcd710f7416ce
Mojtaba Arezoomand <mojtaba.arezoomand@snapp.cab>
2022-06-15 04:01:51 +0430

    fix: remove debug=True

--- main.py
+++ main.py
@@ -387,4 +387,4 @@ async def save_file(upload_file: UploadFile) -> str:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app=app, host="0.0.0.0", port=8080, debug=True)
+    uvicorn.run(app=app, host="0.0.0.0", port=8080)
